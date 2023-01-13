8cd9efcb7cdf059d1de7d501a303d0519e446829
Aleksander Kamiński <aleksander.kaminski@robotec.ai>
2023-01-13 15:51:06 +0100

    Update README.md and fix cmake issues
    Signed-off-by: Aleksander Kamiński <aleksander.kaminski@robotec.ai>

--- Code/FindRGL.cmake
+++ Code/FindRGL.cmake
@@ -11,17 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set(RGL_HASH e113be2206fe27e1434ab6337c932c1d6aaa7afe)
-set(RGL_TAG v0.11.2)
+set(RGL_HASH e5ea8a1a74c253a304f4d1d4652391bca9d448e6)
+set(RGL_TAG v0.11.3)
 
 set(RGL_SO_URL https://github.com/RobotecAI/RobotecGPULidar/releases/download/${RGL_TAG}/libRobotecGPULidar.so)
 set(RGL_API_URL https://raw.githubusercontent.com/RobotecAI/RobotecGPULidar/${RGL_HASH}/include/rgl/api/core.h)
 
-set(RGL_LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3-rdParty/RobotecGPULidar)
+set(RGL_LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdParty/RobotecGPULidar)
 set(RGL_INCLUDE_DIR ${RGL_LIB_DIR}/include)
 
 set(RGL_SO_PATH ${RGL_LIB_DIR}/libRobotecGPULidar.so)
 set(RGL_API_PATH ${RGL_LIB_DIR}/include/rgl/api/core.h)
+
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
 file(DOWNLOAD
         ${RGL_SO_URL}
