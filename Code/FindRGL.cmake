dcd407f16e79b5e73c56f2a3fccf2d7b95147356
Mateusz Szczygielski <mateusz.szczygielski@robotec.ai>
2024-09-27 11:59:19 +0200

    Simplify FindRGL.cmake (use version from development branch)

--- Code/FindRGL.cmake
+++ Code/FindRGL.cmake
@@ -11,29 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 set(RGL_VERSION 0.15.0)
 set(RGL_TAG v${RGL_VERSION})
 
-set(RGL_LINUX_ZIP_URL https://github.com/RobotecAI/RobotecGPULidar/releases/download/${RGL_TAG}/Linux-x64.zip)
-set(RGL_SRC_ZIP_URL https://github.com/RobotecAI/RobotecGPULidar/archive/refs/tags/${RGL_TAG}.zip)
-
 set(RGL_LINUX_ZIP_FILENAME_BASE Linux-x64)
-set(RGL_SRC_ZIP_FILENAME_BASE RobotecGPULidar-${RGL_VERSION})
-
 set(RGL_LINUX_ZIP_FILENAME ${RGL_LINUX_ZIP_FILENAME_BASE}.zip)
-set(RGL_SRC_ZIP_FILENAME ${RGL_SRC_ZIP_FILENAME_BASE}.zip)
+
+set(RGL_LINUX_ZIP_URL https://github.com/RobotecAI/RobotecGPULidar/releases/download/${RGL_TAG}/${RGL_LINUX_ZIP_FILENAME})
+set(RGL_SRC_ROOT_URL https://raw.githubusercontent.com/RobotecAI/RobotecGPULidar/${RGL_TAG})
 
 set(DEST_SO_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdParty/RobotecGPULidar)
 set(DEST_API_DIR ${DEST_SO_DIR}/include/rgl/api)
-set(DEST_API_EXT_DIR ${DEST_API_DIR}/extensions)
 
 set(SO_FILENAME libRobotecGPULidar.so)
-set(API_FILENAME core.h)
-set(API_EXT_ROS_FILENAME ros2.h)
 
 # Paths relative to the .zip file root.
 set(SO_REL_PATH ${SO_FILENAME})
-set(API_REL_PATH RobotecGPULidar-${RGL_VERSION}/include/rgl/api)
 
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
@@ -48,41 +42,37 @@ if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/DOWNLOAD_RGL)
     file(DOWNLOAD
             ${RGL_LINUX_ZIP_URL}
             ${DEST_SO_DIR}/${RGL_LINUX_ZIP_FILENAME}
-            )
+    )
 
     file(DOWNLOAD
-            ${RGL_SRC_ZIP_URL}
-            ${DEST_API_DIR}/${RGL_SRC_ZIP_FILENAME}
-            )
+            ${RGL_SRC_ROOT_URL}/include/rgl/api/core.h
+            ${DEST_API_DIR}/core.h
+    )
+
+    file(DOWNLOAD
+            ${RGL_SRC_ROOT_URL}/extensions/ros2/include/rgl/api/extensions/ros2.h
+            ${DEST_API_DIR}/extensions/ros2.h
+    )
 
     # Extract the contents of the downloaded archive files
     file(ARCHIVE_EXTRACT INPUT ${DEST_SO_DIR}/${RGL_LINUX_ZIP_FILENAME}
             DESTINATION ${DEST_SO_DIR}
             PATTERNS ${SO_REL_PATH}
             VERBOSE
-            )
-
-    file(ARCHIVE_EXTRACT INPUT ${DEST_API_DIR}/${RGL_SRC_ZIP_FILENAME}
-            DESTINATION ${DEST_API_DIR}
-            PATTERNS ${API_REL_PATH}/*
-            VERBOSE
-            )
+    )
 
     # Move the extracted files to their desired locations
     file(RENAME ${DEST_SO_DIR}/${SO_REL_PATH} ${DEST_SO_DIR}/${SO_FILENAME})
-    file(COPY ${DEST_API_DIR}/${API_REL_PATH} DESTINATION ${DEST_SO_DIR}/include/rgl/)
 
     # Remove the unwanted byproducts
-    file(REMOVE_RECURSE ${DEST_API_DIR}/${RGL_SRC_ZIP_FILENAME_BASE})
-    file(REMOVE ${DEST_API_DIR}/${RGL_SRC_ZIP_FILENAME})
     file(REMOVE ${DEST_SO_DIR}/${RGL_LINUX_ZIP_FILENAME})
 
     file(REMOVE ${CMAKE_CURRENT_SOURCE_DIR}/DOWNLOAD_RGL)
 else ()
     message(WARNING "Omitting the RobotecGPULidar library download. This is intended when using the Clion multi-profile"
-    " project reload. This may also happen due to interruption of previous project configurations. If you have"
-    " any issues related to the libRobotecGPULidar.so file please make sure that the DOWNLOAD_RGL file is not"
-    " present under the Code directory."
+            " project reload. This may also happen due to interruption of previous project configurations. If you have"
+            " any issues related to the libRobotecGPULidar.so file please make sure that the DOWNLOAD_RGL file is not"
+            " present under the Code directory."
     )
 endif ()
 
