4db7c3825b359663a33f058d1676953ce20f9b03
Mateusz Szczygielski <112629916+msz-rai@users.noreply.github.com>
2026-03-20 14:19:39 +0100

    Update RGL (native library) version (v0.21.0) (#87)
    * Update RGL (native library) version (v0.21.0)
    * Update README
    * Bump Gem version 1.2.1 -> 1.2.2
    
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>
    Co-authored-by: Mateusz Szczygielski <mateusz.szczygielski@robotec.ai>
    Co-authored-by: Jan Hanca <jan.hanca@robotec.ai>

--- Code/FindRGL.cmake
+++ Code/FindRGL.cmake
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set(RGL_VERSION 0.19.0)
+set(RGL_VERSION 0.21.0)
 set(RGL_TAG v${RGL_VERSION})
 
 # Metadata files used to determine if RGL download is required
